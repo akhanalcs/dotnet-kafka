5184eeb526ff749eb2e5ba37895589d30d2b4c61
Ashish Khanal <affableashk@outlook.com>
2024-02-13 13:16:56 -0500

    Add appsettings.json to console app (Consumer)

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using var host = Host.CreateApplicationBuilder(args).Build();
+var config = host.Services.GetRequiredService<IConfiguration>();
+
+var kafkaBootstrapServer = config["Kafka:BootstrapServer"];
+
+Console.WriteLine("Bootstrap Server: {0}", kafkaBootstrapServer);
+Console.ReadLine();
