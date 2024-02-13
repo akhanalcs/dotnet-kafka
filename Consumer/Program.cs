9c8b614ab6b667b86e5009d939ebac8f2fd253ff
Ashish Khanal <affableashk@outlook.com>
2024-02-13 15:16:38 -0500

    Add appsettings for both Producer and Consumer

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,13 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Confluent.Kafka;
 
 using var host = Host.CreateApplicationBuilder(args).Build();
 var config = host.Services.GetRequiredService<IConfiguration>();
 
-var kafkaBootstrapServer = config["Kafka:BootstrapServer"];
+var consumerConfig = config.GetSection("KafkaConsumer").Get<ConsumerConfig>();
 
-Console.WriteLine("Bootstrap Server: {0}", kafkaBootstrapServer);
 Console.ReadLine();
