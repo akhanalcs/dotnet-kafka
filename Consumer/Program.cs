b58d789e7cacb7c9023771ba6f1c6e3bb51fc9c6
Ashish Khanal <affableashk@outlook.com>
2024-02-13 20:47:06 -0500

    Sets up the config to be read from both appsettings.json and secrets.json (which holds API keys and secrets of Kafka cluster)

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Confluent.Kafka;
 
 using var host = Host.CreateApplicationBuilder(args).Build();
-var config = host.Services.GetRequiredService<IConfiguration>();
 
-var consumerConfig = config.GetSection("KafkaConsumer").Get<ConsumerConfig>();
+//var config = host.Services.GetRequiredService<IConfiguration>(); // This works like below EXCEPT the user secrets part.
+var config = new ConfigurationBuilder()
+  .AddJsonFile("appsettings.json")
+  .AddEnvironmentVariables()
+  .AddUserSecrets<Program>()
+  .Build();
+
+var consumerConfig = config.GetSection("KafkaConsumer").Get<ConsumerConfig>()!;
 
 Console.ReadLine();
